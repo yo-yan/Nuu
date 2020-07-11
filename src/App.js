25ac84f38cfbc5d906aac8e58441cd28ae80a460
橋本洋介 <hashimotoyousuke@hashimoukenoAir.elecom>
2020-07-12 00:12:17 +0900

    EXTRA-1

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ function App() {
 
   const increment = () => {
     setCount(count + 1);
-    console.log(count);
+
+  }
+  const decrement = () => {
+    setCount(count - 1);
+
+  }
+  const recrement = () => {
+    setCount(0);
+
   }
 
 
   return (
     <div>
-      <button onClick={increment}>up</button>
+      <button onClick={increment}>いいね！</button>
+
+      <button onClick={decrement}>良くないね！</button>
+
+      <button onClick={recrement}>リセット</button>
+
+      {count}
 
 
 
