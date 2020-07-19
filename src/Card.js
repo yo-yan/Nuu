e8c19ea8688007c824ac7aa53945c5f75f7ac32f
橋本洋介 <hashimotoyousuke@iMac.local>
2020-07-19 17:29:03 +0900

    NOMAL-1

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,9 @@ const Card = (props) => {
 
     return (
         <div className='wrapper'>
-            <ul>
-                <h1>id:{props.id}</h1>
-                <h1>name:{props.name}</h1>
-                <h1>date:{props.date}</h1>
-            </ul>
+            <h1>id:{props.id}</h1>
+            <h1>name:{props.name}</h1>
+            <h1>date:{props.date}</h1>
         </div>
     );
 };
