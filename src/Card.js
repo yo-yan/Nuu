45dae1ad698204eda34836e46562f874f2617726
橋本洋介 <hashimotoyousuke@iMac.local>
2020-07-26 01:16:44 +0900

    NOMAL-2

--- src/Card.js
+++ src/Card.js
@@ -3,21 +3,12 @@ import './Card.css';
 
 const Card = (props) => {
 
-
-
     return (
-
-
-
-
         <tr>
             <td>{props.id}</td>
             <td>{props.name}</td>
             <td>{props.date}</td>
         </tr>
-
-
-
     );
 };
 
--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const ComponentA = () => {
 
@@ -12,6 +13,20 @@ const ComponentA = () => {
                 <li><Link to='/componentb' >ComponentB</Link></li>
                 <li><Link to='/componentc' >ComponentC</Link></li>
             </ul>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
             <Table striped bordered hover>
                 <thead>
                     <tr>
